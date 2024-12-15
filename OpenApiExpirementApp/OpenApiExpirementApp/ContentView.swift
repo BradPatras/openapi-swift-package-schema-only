b25c7e9048364e6ee982566fce86a0ce4a084220
Brad <BradPatras@users.noreply.github.com>
2024-12-15 16:50:25 -0600

    Trying a static func that return a client

--- OpenApiExpirementApp/OpenApiExpirementApp/ContentView.swift
+++ OpenApiExpirementApp/OpenApiExpirementApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
 	@State var message: String?
-	let client: Client? = nil
+	let client: Client?
 
     var body: some View {
         VStack {
--- Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
+++ Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
@@ -2,19 +2,15 @@ import Foundation
 import OpenAPIRuntime
 import OpenAPIURLSession
 
-private struct GeneratorHelper {
-	let client: Client
-}
-
 // If this library package doesn't reference the Generated API in some way, then the consumers of this library won't be able to access it either.
 public extension Client {
-	init(
+	static func getClient(
 		serverURL: Foundation.URL,
 		configuration: Configuration = .init(),
 		middlewares: [any ClientMiddleware] = [],
 		urlSessionConfiguration: URLSessionTransport.Configuration = .init()
-	) {
-		self.init(
+	) -> Client {
+		.init(
 			serverURL: serverURL,
 			configuration: configuration,
 			transport: URLSessionTransport(),
