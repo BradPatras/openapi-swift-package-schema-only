1d2c3089d9bf5ac8153f5c50f4086ebaa8b2b0c7
Brad <BradPatras@users.noreply.github.com>
2024-12-15 16:47:25 -0600

    adding private struct to hopefully cause api to be generated

--- Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
+++ Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
@@ -2,6 +2,10 @@ import Foundation
 import OpenAPIRuntime
 import OpenAPIURLSession
 
+private struct GeneratorHelper {
+	let client: Client
+}
+
 // If this library package doesn't reference the Generated API in some way, then the consumers of this library won't be able to access it either.
 public extension Client {
 	init(
