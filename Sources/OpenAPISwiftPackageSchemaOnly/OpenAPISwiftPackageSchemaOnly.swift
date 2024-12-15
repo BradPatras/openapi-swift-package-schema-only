addb9f60bc0fbac603f580cb7e9e58307eb25834
Brad <BradPatras@users.noreply.github.com>
2024-12-15 16:44:22 -0600

    Added public extension on Client to hopefully avoid the consumers needing to import URLSessionTransport and also kick-starting the api generator when building

--- Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
+++ Sources/OpenAPISwiftPackageSchemaOnly/OpenAPISwiftPackageSchemaOnly.swift
@@ -1,2 +1,20 @@
-// Empty swift file needed for successful swift package compliation
-struct Something {}
+import Foundation
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+// If this library package doesn't reference the Generated API in some way, then the consumers of this library won't be able to access it either.
+public extension Client {
+	init(
+		serverURL: Foundation.URL,
+		configuration: Configuration = .init(),
+		middlewares: [any ClientMiddleware] = [],
+		urlSessionConfiguration: URLSessionTransport.Configuration = .init()
+	) {
+		self.init(
+			serverURL: serverURL,
+			configuration: configuration,
+			transport: URLSessionTransport(),
+			middlewares: middlewares
+		)
+	}
+}
